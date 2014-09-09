3cfc9efae34d1bf2f71504b649e8a3c812f49b41
Jakob Westhoff <jakob@westhoffswelt.de>
2014-09-09 14:11:47 +0200

    Added solarized dircolors

--- checkout_dependencies.sh
+++ checkout_dependencies.sh
@@ -1,6 +1,27 @@
+#!/bin/bash
+function checkout_or_update() {
+    local repository="${1}"
+    local target="${2}"
+
+    if [ -d "${target}" ]; then
+        pushd "${target}"
+        git reset --hard
+        git pull -f
+        popd
+    else
+        git clone "${repository}" "${target}"
+    fi
+}
+
 pushd ~
 echo "Installing bash configuration dependencies"
-git clone https://github.com/jakobwesthoff/colorizer.git .colorizer
+checkout_or_update https://github.com/jakobwesthoff/colorizer.git .colorizer
+
+echo
 echo "Installing autojump"
-git clone https://github.com/joelthelion/autojump.git .autojump
+checkout_or_update https://github.com/joelthelion/autojump.git .autojump
+
+echo
+echo "Installing solarized dircolors"
+checkout_or_update https://github.com/seebi/dircolors-solarized.git .dircolors-solarized
 popd
