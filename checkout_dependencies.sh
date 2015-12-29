da8845fc428553b912dc72883efe04a86468146f
Jakob Westhoff <jakob@westhoffswelt.de>
2015-12-29 18:07:48 +0100

    Added zgen

--- checkout_dependencies.sh
+++ checkout_dependencies.sh
@@ -21,8 +21,13 @@ echo
 echo "Installing autojump"
 checkout_or_update https://github.com/joelthelion/autojump.git .autojump
 
+echo
 echo "Installing r9e-bashrc"
 checkout_or_update https://github.com/jakobwesthoff/r9e-bashrc.git .r9e-bashrc
+
+echo
+echo "Installing zgen"
+checkout_or_update https://github.com/tarjoilija/zgen.git .zgen
 
 echo
 echo "Installing solarized dircolors"
