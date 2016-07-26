0a8e4760b0f5dee9bb9cbf4f086d18ced74f707a
Jakob Westhoff <jakob@westhoffswelt.de>
2016-07-26 13:36:05 +0200

    Lazy load nvm only when needed

--- zsh/zshrc.d/nvm.sh
+++ zsh/zshrc.d/nvm.sh
@@ -1,6 +1,24 @@
-if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
-  source "$HOME/.nvm/nvm.sh"
-fi
+# Lazy load nvm
+nvm() {
+    unset -f nvm
+    export NVM_DIR="${HOME}/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    nvm "$@"
+}
+ 
+node() {
+    unset -f node
+    export NVM_DIR="${HOME}/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    node "$@"
+}
+ 
+npm() {
+    unset -f npm
+    export NVM_DIR="${HOME}/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    npm "$@"
+}
 
 ## $ npm completion
 
