0ddac3423438c50bece71e266bbca85652799592
Jakob Westhoff <jakob@westhoffswelt.de>
2017-02-03 12:40:39 +0100

    Vagrant cache will be updated if machine is rebooted

--- zsh/zshrc.d/vagrant.sh
+++ zsh/zshrc.d/vagrant.sh
@@ -3,10 +3,27 @@
 # quite some time, which is kind of annoying upon each new shell instantiation.
 
 _VAGRANT_RUNNING_CACHE="${HOME}/.vagrant_running_cache"
+_VAGRANT_RUNNING_CACHE_BOOTTIME="${HOME}/.vagrant_running_cache_boottime"
 _VAGRANT_EXECUTABLE="$(which vagrant)"
+
+function _vagrant_get_cached_boottime() {
+    local boottime="0"
+    if [ -f "${_VAGRANT_RUNNING_CACHE_BOOTTIME}" ]; then
+        boottime="$(cat "${_VAGRANT_RUNNING_CACHE_BOOTTIME}")"
+    fi
+    echo -n "${boottime//[$'\t\r\n ']}"
+}
+
+function _vagrant_get_boottime() {
+    local full_boot_time="$(sysctl -a|grep kern.boottime)"
+    local isolatedBootTime="${full_boot_time#*sec = }"
+    isolatedBootTime="${isolatedBootTime%%,*}"
+    echo -n "${isolatedBootTime//[$'\t\r\n ']}"
+}
 
 function _vagrant_update_cache() {
     "${_VAGRANT_EXECUTABLE}" global-status 2>&1 >"${_VAGRANT_RUNNING_CACHE}"
+    _vagrant_get_boottime >"${_VAGRANT_RUNNING_CACHE_BOOTTIME}"
 }
 
 function _vagrant_read_cache() {
@@ -18,10 +35,13 @@ function _vagrant_clear_cache() {
     if [ -f "${_VAGRANT_RUNNING_CACHE}" ]; then
         rm -f "${_VAGRANT_RUNNING_CACHE}"
     fi
+    if [ -f "${_VAGRANT_RUNNING_CACHE_BOOTTIME}" ]; then
+        rm -f "${_VAGRANT_RUNNING_CACHE_BOOTTIME}"
+    fi
 }
 
 function _vagrant_get_status() {
-    if [ ! -f "${_VAGRANT_RUNNING_CACHE}" ]; then
+    if [ ! -f "${_VAGRANT_RUNNING_CACHE}" ] || [ "$(_vagrant_get_cached_boottime)" -lt "$(_vagrant_get_boottime)" ]; then
         _vagrant_update_cache
     fi
     _vagrant_read_cache
