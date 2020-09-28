3c4e904b5937e8df9a9eeea4cbdac4663a9461a2
Jakob Westhoff <jakob@westhoffswelt.de>
2020-09-28 14:56:46 +0200

    Enforce param

--- zsh/zshrc.d/050_kubectl.sh
+++ zsh/zshrc.d/050_kubectl.sh
@@ -2,5 +2,10 @@
 alias k="kubectl"
 
 kns() {
+    if [ $# -ne 1 ]; then
+        echo "Please provide a new namespace to switch to."
+        return 1
+    fi
+
     kubectl config set-context "$(kubectl config current-context)" --namespace="$1"
 }
