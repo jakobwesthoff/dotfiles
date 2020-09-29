3512ee7faad706dc202d70f3b22d4582638fd7f0
Jakob Westhoff <jakob@westhoffswelt.de>
2020-09-29 12:01:48 +0200

    added some more kubectl helpers

--- zsh/zshrc.d/050_kubectl.sh
+++ zsh/zshrc.d/050_kubectl.sh
@@ -1,4 +1,24 @@
 # Kubernetes
+
+##
+# Internals
+##
+
+__kubectl_select_container() {
+    local podName="$1"
+    local containerCount="$(kubectl get pods "$podName" -o=jsonpath='{range .spec.containers[*]}{.name}{"\n"}{end}'|wc -l)"
+
+    if [ $containerCount -eq 1 ]; then
+        kubectl get pods "$podName" -o=jsonpath='{range .spec.containers[*]}{.name}{end}'
+    else
+        kubectl get pods "$podName" -o=jsonpath='{range .spec.containers[*]}{.name}{"\n"}{end}'|fzf
+    fi
+}
+
+
+##
+# Public
+##
 alias k="kubectl"
 
 kns() {
@@ -8,4 +28,27 @@ kns() {
     fi
 
     kubectl config set-context "$(kubectl config current-context)" --namespace="$1"
+}
+
+krsh() {
+    if [ $# -ne 1 ]; then
+        echo "Please provide a pod name to rsh into."
+        return 1
+    fi
+
+    local container="$(__kubectl_select_container "$1")"
+    kubectl exec --stdin --tty "$1" -c "$container" -- /bin/bash
+}
+
+klog() {
+    if [ $# -lt 1 ]; then
+        echo "Please provide a pod name acquire log from."
+        return 1
+    fi
+
+    local pod="$1"
+    shift 1
+
+    local container="$(__kubectl_select_container "$pod")"
+    kubectl logs "$pod" -c "$container" "$@"
 }
