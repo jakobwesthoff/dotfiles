26cbce38fe99bd3d87ba86940e9d0174dbc65e8c
Jakob Westhoff <jakob@westhoffswelt.de>
2024-03-05 17:23:24 +0100

    Disabled the fancy notifications

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   -- Disable the fancy cmdline, messages and popups
   { "folke/noice.nvim", enabled = false },
+  -- Disable those fancy overlay notifications in the top right corner
+  { "rcarriga/nvim-notify", enabled = false },
 }
