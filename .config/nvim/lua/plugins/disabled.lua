7d0d3000947398cdb6c3babb65381da737392fb4
Jakob Westhoff <jakob@westhoffswelt.de>
2024-03-05 23:08:48 +0100

    Add missing plugin disable comment

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,5 +3,6 @@ return {
   { "folke/noice.nvim", enabled = false },
   -- Disable those fancy overlay notifications in the top right corner
   { "rcarriga/nvim-notify", enabled = false },
+  -- Disable strange pseudo markup headline rendering in eg. markdown
   { "lukas-reineke/headlines.nvim", enabled = false },
 }
