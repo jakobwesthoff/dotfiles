8a84e5eb0fc2b96c16544353bb2c6ad642626cd3
Jakob Westhoff <jakob@westhoffswelt.de>
2024-05-26 21:49:02 +0200

    Fixed disabling of inlay hints

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -17,13 +17,6 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "plugins" },
   },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      -- Disable inlay hints by default
-      inlay_hints = { enabled = false },
-    },
-  },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
---
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- Disable inlay hints by default
+      inlay_hints = { enabled = false },
+    },
+  }
+}
