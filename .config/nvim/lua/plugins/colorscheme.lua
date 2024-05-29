0526803966383c844c56811c78af68ccb4b2037b
Jakob Westhoff <jakob@westhoffswelt.de>
2024-05-29 13:24:24 +0200

    Some slight theme tweakes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -26,7 +26,7 @@ return {
     config = function()
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
-      vim.g.gruvbox_material_background = "medium"
+      vim.g.gruvbox_material_background = "hard"
       vim.g.gruvbox_material_enable_italic = true
       vim.cmd.colorscheme('gruvbox-material')
     end
