b2f2c4f932bf023168a5601c4b88dd81ec0e3af4
Jakob Westhoff <jakob@westhoffswelt.de>
2025-01-18 12:52:34 +0100

    Disable crates cmp integration and use in-process language server

--- .config/nvim/lua/mrjakob/plugins/crates.lua
+++ .config/nvim/lua/mrjakob/plugins/crates.lua
@@ -5,9 +5,21 @@ return {
   event = { "BufRead Cargo.toml" },
   config = function()
     require("crates").setup({
+      lsp = {
+        enabled = true,
+        -- Handled by our LspAttach Autocommand
+        -- on_attach = function(client, bufnr)
+        --   -- the same on_attach function as for your other lsp's
+        -- end,
+        actions = true,
+        completion = true,
+        hover = true,
+      },
       completion = {
-        cmp = {
+        crates = {
           enabled = true,
+          max_results = 8,
+          min_chars = 3,
         },
       },
     })
