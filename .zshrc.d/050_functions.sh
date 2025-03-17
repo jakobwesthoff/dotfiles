3c635792372566c4c7742afea8cd0892f0390d04
Jakob Westhoff <jakob@westhoffswelt.de>
2025-03-17 09:51:52 +0100

    Add functions ghclone and glclone cloning automatically into correct dir
    Formatted file in the process

--- .zshrc.d/050_functions.sh
+++ .zshrc.d/050_functions.sh
@@ -2,11 +2,11 @@
 #
 # Usage: "flait logfile"
 railf() {
-    while true;do
+    while true; do
         clear
-        tail -n $(($LINES-1)) $1 \
-        | tac \
-        | cut -c 1-$COLUMNS
+        tail -n $(($LINES - 1)) $1 |
+            tac |
+            cut -c 1-$COLUMNS
         sleep 1
     done
 }
@@ -41,12 +41,12 @@ mkcdd() {
 # Define a more convinient way of extracting colums using awk
 # Usage column <colnumber> [<optional_column seperator>] < infile
 col() {
-    eval awk ${2:+"-F $2"} "'{print \$$1}'";
+    eval awk ${2:+"-F $2"} "'{print \$$1}'"
 }
 
 # In honor to Star-Trek
 makeitso() {
-    sudo $(history 2|head -n 1|sed -e 's@^[0-9]\+\s\+@@')
+    sudo $(history 2 | head -n 1 | sed -e 's@^[0-9]\+\s\+@@')
 }
 
 # Copy current working directory to clipboard AND output it
@@ -85,8 +85,7 @@ gitmvcase() {
     git mv "${source}.${uuid}" "${destination}"
 }
 
-wait_for()
-{
+wait_for() {
     if [ ${#} -ne 1 ]; then
         echo "usage: ${FUNCNAME[0]} <hostname>"
         return 1
@@ -98,7 +97,6 @@ wait_for()
     if [ "$(uname)" = "Darwin" ]; then
         wait_option='-t'
     fi
-
 
     local nl=''
     while ! ping -qc1 "${wait_option}2" "${host}" >/dev/zero 2>&1; do
@@ -114,8 +112,7 @@ wait_for()
 compdef _hosts wait_for
 compdef _known_hosts wait_for
 
-wait_for_port()
-{
+wait_for_port() {
     if [ ${#} -ne 2 ]; then
         echo "usage: ${FUNCNAME[0]} <hostname> <port>"
         return 1
@@ -133,13 +130,12 @@ wait_for_port()
         nl='\n'
     done
 
-   printf "${nl}port %s on %s is now available\n" "${port}" "${host}"
+    printf "${nl}port %s on %s is now available\n" "${port}" "${host}"
 }
 compdef _hosts wait_for_port
 compdef _known_hosts wait_for_port
 
-wait_for_ssh()
-{
+wait_for_ssh() {
     local host="$(ssh -G "${@}" | grep '^hostname ' | sed 's/^hostname //')"
     local port="$(ssh -G "${@}" | grep '^port ' | sed 's/^port //')"
 
@@ -147,8 +143,7 @@ wait_for_ssh()
     ssh "${@}"
 }
 
-_wait_for_ssh()
-{
+_wait_for_ssh() {
     local service='ssh'
     _ssh
 }
@@ -162,10 +157,10 @@ retag() {
     fi
     local TAG="$1"
     git push --delete origin "$TAG"
-    git tag -d "$TAG";
-    git tag "$TAG";
-    git push --tags;
- }
+    git tag -d "$TAG"
+    git tag "$TAG"
+    git push --tags
+}
 
 rmtag() {
     if [ "$#" -ne "1" ]; then
@@ -186,9 +181,9 @@ push_nexus() {
     local image="$1"
     local tag="$2"
 
-    docker pull registry.gitlab.com/ekkogmbh/artifacts/${image}:${tag} && \
-    docker image tag registry.gitlab.com/ekkogmbh/artifacts/${image}:${tag} lpitdnexus01.bmwgroup.net:16052/epaper/${image}:${tag}  && \
-    docker push lpitdnexus01.bmwgroup.net:16052/epaper/${image}:${tag}
+    docker pull registry.gitlab.com/ekkogmbh/artifacts/${image}:${tag} &&
+        docker image tag registry.gitlab.com/ekkogmbh/artifacts/${image}:${tag} lpitdnexus01.bmwgroup.net:16052/epaper/${image}:${tag} &&
+        docker push lpitdnexus01.bmwgroup.net:16052/epaper/${image}:${tag}
 }
 
 # Allow to "Yoink" something directly from my commandline either by specifying it or using fzf in the current directory
@@ -196,9 +191,97 @@ yoink() {
     if [ "$#" -gt 0 ]; then
         open -a Yoink "$@"
     else
-        local result="$(fzf)"
+        local result
+        result="$(fzf)"
         if [ -n "${result}" ]; then
             open -a Yoink "${result}"
         fi
     fi
+}
+
+## Generic function to clone github or gitlab repositories to the correct dev folder on *my* system
+## which currently is ~/Development/<github|gitlab>/<org|author>/<reponame>
+##
+## The function is usually called using the aliases ghclone and glclone
+gitclone() {
+    if [ "$#" -lt 2 ]; then
+        echo "gitclone <gitlab|github> [--ssh] <ssh-url|http(s)-url|org/name>"
+        return 1
+    fi
+
+    local ssh=""
+    local mode=""
+    local repo=""
+
+    while [ "$#" -gt 0 ]; do
+        if [[ "$1" == "--ssh" ]]; then
+            ssh="true"
+            shift
+            continue
+        fi
+
+        if [ -z "$mode" ]; then
+            mode="$1"
+            shift
+            continue
+        fi
+
+        if [ -z "$repo" ]; then
+            repo="$1"
+            shift
+            continue
+        fi
+
+        break
+    done
+
+    # Extract org/name
+    local org=""
+    local name=""
+    if [[ "${repo}" =~ ^https?://.+/(.+)/(.+)\.git$ ]]; then
+        org="${match[1]}"
+        name="${match[2]}"
+    elif [[ "${repo}" =~ ^git@.+:(.+)/(.+)\.git$ ]]; then
+        org="${match[1]}"
+        name="${match[2]}"
+    elif [[ "${repo}" =~ ^([^/]+)/([^/]+)$ ]]; then
+        org="${match[1]}"
+        name="${match[2]}"
+    else
+        echo "The given repository does not seem to be an http(s), ssh or org/name identifier: $repo"
+        return 1
+    fi
+
+    if [[ "$mode" != "github" ]] && [[ "$mode" != "gitlab" ]]; then
+        echo "Unsupported mode: $mode"
+        return 1
+    fi
+
+    local target_without_name="${HOME}/Development/${mode}/${org}"
+    local target="${target_without_name}/${name}"
+
+    if [ -d "$target" ]; then
+        echo "Target directory already exists. Just changing into it: ${target}"
+        cd "${target}" || return 1
+        return 0
+    fi
+
+    local url=""
+    if [ -n "${ssh}" ]; then
+        url="git@${mode}.com:${org}/${name}.git"
+    else
+        url="https://${mode}.com/${org}/${name}.git"
+    fi
+
+    mkcd "${target_without_name}"
+    git clone "${url}" "${name}"
+    cd "${name}" || return 1
+}
+
+ghclone() {
+    gitclone github "$@"
+}
+
+glclone() {
+    gitclone gitlab "$@"
 }
