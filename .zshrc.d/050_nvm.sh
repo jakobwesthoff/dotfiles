d53d66aa9f666b40d8c3819e4ca2ae796d10d141
Jakob Westhoff <jakob@westhoffswelt.de>
2025-05-05 10:27:35 +0200

    Use default node installation by default. Only lazyload nvm command.

--- .zshrc.d/050_nvm.sh
+++ .zshrc.d/050_nvm.sh
@@ -2,34 +2,34 @@ export NVM_DIR="/opt/homebrew/opt/nvm"
 # Lazy load nvm
 _nvm_lazy_load_unset() {
     unset -f nvm
-    unset -f node
-    unset -f npm
-    unset -f npx
+    # unset -f node
+    # unset -f npm
+    # unset -f npx
 }
 
 nvm() {
     _nvm_lazy_load_unset
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
     nvm "$@"
 }
- 
-node() {
-    _nvm_lazy_load_unset
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-    node "$@"
-}
- 
-npm() {
-    _nvm_lazy_load_unset
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-    npm "$@"
-}
 
-npx() {
-    _nvm_lazy_load_unset
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-    npx "$@"
-}
+# node() {
+#     _nvm_lazy_load_unset
+#     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+#     node "$@"
+# }
+#
+# npm() {
+#     _nvm_lazy_load_unset
+#     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+#     npm "$@"
+# }
+#
+# npx() {
+#     _nvm_lazy_load_unset
+#     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+#     npx "$@"
+# }
 
 ## $ npm completion
 
@@ -41,14 +41,14 @@ npx() {
 # Or, maybe: npm completion > /usr/local/etc/bash_completion.d/npm
 #
 
-  _npm_completion() {
+_npm_completion() {
     local si=$IFS
-    compadd -- $(COMP_CWORD=$((CURRENT-1)) \
-                 COMP_LINE=$BUFFER \
-                 COMP_POINT=0 \
-                 npm completion -- "${words[@]}" \
-                 2>/dev/null)
+    compadd -- $(COMP_CWORD=$((CURRENT - 1)) \
+        COMP_LINE=$BUFFER \
+        COMP_POINT=0 \
+        npm completion -- "${words[@]}" \
+        2>/dev/null)
     IFS=$si
-  }
-  compdef _npm_completion npm
+}
+compdef _npm_completion npm
 ###-end-npm-completion-###
