26f210f85bfd44559bde077b3aa455a730dbb77e
Jakob Westhoff <jakob@westhoffswelt.de>
2025-10-09 23:41:36 +0200

    Replaced project-nvim with our own taproot.nvim plugin
    We were using project-nvim only for its automatic root detection. As
    project-nvim, was getting "old" (due to not being maintained anymore),
    and caused deprecated methods to be called, I decided to rewrite a
    simple root detection, after I couldn't find a replacement really doing
    what I wanted. I published it as taproot.nvim, and am now using it here.

--- .config/nvim/lua/mrjakob/plugins/project.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "ahmedkhalf/project.nvim",
-  main = "project_nvim",
-  opts = {},
-  priority = 900,
-  enabled = false,
-}
---
+++ .config/nvim/lua/mrjakob/plugins/taproot.lua
@@ -0,0 +1,4 @@
+return {
+  "jakobwesthoff/taproot.nvim",
+  opts = {},
+}
