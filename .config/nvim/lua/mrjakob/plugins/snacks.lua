82dfe46c28decc9dbd051be3bc10d44d25d67c73
Jakob Westhoff <jakob@westhoffswelt.de>
2026-03-20 14:59:39 +0100

    Enable snacks.input with cursor position workaround
    snacks.input uses a prompt buffer where `startinsert!` doesn't
    reliably place the cursor after pre-filled default text (e.g. LSP
    rename). Work around this by forcing the cursor to end-of-line via
    a scheduled `FileType` autocmd on `snacks_input` buffers.

--- .config/nvim/lua/mrjakob/plugins/snacks.lua
+++ .config/nvim/lua/mrjakob/plugins/snacks.lua
@@ -2,7 +2,7 @@ return {
   "folke/snacks.nvim",
   ---@type snacks.Config
   opts = {
-    -- input = { enabled = true }, -- cursor positioning broken with prompt buffers
+    input = { enabled = true },  -- workaround for cursor position below
     indent = {
       enabled = true,
       animate = {
@@ -30,4 +30,21 @@ return {
       end,
     },
   },
+  config = function(_, opts)
+    require("snacks").setup(opts)
+
+    -- Workaround: snacks.input uses a prompt buffer where `startinsert!`
+    -- doesn't reliably place the cursor after pre-filled default text.
+    -- Force cursor to end of line once the input window opens.
+    -- TODO: Remove once fixed upstream in folke/snacks.nvim
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "snacks_input",
+      callback = function()
+        vim.schedule(function()
+          local line = vim.api.nvim_get_current_line()
+          vim.api.nvim_win_set_cursor(0, { 1, #line })
+        end)
+      end,
+    })
+  end,
 }
