61c4a6c1e382f65abcff84bac1955ee8ba662516
Jakob Westhoff <jakob@westhoffswelt.de>
2026-06-14 16:42:03 +0200

    Remove LSP document-highlight on cursor hold
    The cursor-hold same-symbol highlighting was unused. Drop the
    documentHighlight CursorHold/CursorMoved autocmds and the LspDetach
    handler that tore them down. The LspAttach callback now wires only the
    inlay-hint toggle.

--- .config/nvim/lua/mrjakob/plugins/lsp.lua
+++ .config/nvim/lua/mrjakob/plugins/lsp.lua
@@ -99,32 +99,7 @@ return {
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
         callback = function(event)
-          -- Highlight references of the word under the cursor on CursorHold,
-          -- clear them on CursorMoved.
           local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-            local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.clear_references,
-            })
-
-            vim.api.nvim_create_autocmd("LspDetach", {
-              group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-              callback = function(event2)
-                vim.lsp.buf.clear_references()
-                vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
-              end,
-            })
-          end
-
           if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
             vim.keymap.set("n", "<leader>uh", function()
               local buf = vim.api.nvim_get_current_buf()
