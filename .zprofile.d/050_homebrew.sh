e6af543645cce179d644d6ea24d9d64e3eb22e8d
Jakob Westhoff <jakob@westhoffswelt.de>
2026-08-06 10:12:03 +0200

    Do not upgrade packages marked auto-update when brew upgrade runs

--- .zprofile.d/050_homebrew.sh
+++ .zprofile.d/050_homebrew.sh
@@ -1,6 +1,9 @@
 # Configure homebrew cask to install to main application directory
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
+# Skip upgrading casks that already self-update
+export HOMEBREW_NO_UPGRADE_AUTO_UPDATES_CASKS=1
+
 # Set HOMEBREW github token
 [ -f "${HOME}/.HOMEBREW_GITHUB_API_TOKEN" ] && source "${HOME}/.HOMEBREW_GITHUB_API_TOKEN"
 export HOMEBREW_GITHUB_API_TOKEN
